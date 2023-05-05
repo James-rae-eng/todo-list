27a89dd12b537589662dee3b51b467292db18245
James Rae <jrae151515@yahoo.co.uk>
2023-05-05 18:42:43 +0100

    Added form to html, created category & todo classes in their own imported modules. Added creation of the default category & a todo upon a submition of the form

---
+++ src/category.js
@@ -0,0 +1,8 @@
+class Category {
+  constructor(title, list = []) {
+    this.title = title;
+    this.list = list;
+  }
+}
+
+export default Category;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 import './style.css';
+import Category from './category';
+import Todo from './todo';
+
+// Initialise variables
+const form = document.getElementById('todoForm');
+
+// create default home category, create active category instance, add switching
+const home = new Category('home');
+let activeCategory = home;
+
+const switchCategory = (newCategory) => {
+  activeCategory = newCategory;
+};
+
+// Create new todo
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formValue = event.target.elements;
+
+  const todo = new Todo(
+    formValue.title.value,
+    formValue.description.value,
+    formValue.dueDate.value,
+    formValue.priority.value,
+  );
+
+  // Push Obj to category array.
+  activeCategory.list.push(todo);
+  // Reset form fields
+  form.reset();
+  // Hide form
+  form.style.display = 'none';
+  // Update the list of books.
+});
+
+const addTodo = document.getElementById('addTodo');
+addTodo.addEventListener('click', () => {
+  if (form.style.display === 'none') {
+    form.style.display = 'block';
+  } else {
+    form.style.display = 'none';
+  }
+});
