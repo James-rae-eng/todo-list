0cf341e8fa4768c6a3833f98d161eccdaf54a66f
James Rae <jrae151515@yahoo.co.uk>
2023-05-07 14:17:58 +0100

    Added formatting of date object in todo class. Added displaying of list of todos in the current catagory in the DOM, as well as an event listener to expand the todos to show the description when the div is clicked

--- src/index.js
+++ src/index.js
@@ -13,6 +13,58 @@ const switchCategory = (newCategory) => {
   activeCategory = newCategory;
 };
 
+// Display list of all todos in catagory on screen
+function listDisplay() {
+  const list = document.getElementById('list');
+  list.innerHTML = '';
+
+  // iterate through catagory
+  activeCategory.list.forEach((element) => {
+    const todo = document.createElement('div');
+    todo.id = 'todo';
+
+    const todoHead = document.createElement('div');
+    todoHead.id = 'todoHead';
+
+    const expanded = document.createElement('div');
+    expanded.id = 'expanded';
+    expanded.style.display = 'none';
+
+    const title = document.createElement('p');
+    title.innerHTML = element.title;
+    todoHead.appendChild(title);
+    const date = document.createElement('p');
+    date.innerHTML = element.formatDate();
+    todoHead.appendChild(date);
+    const priority = document.createElement('p');
+    priority.innerHTML = element.priority;
+    todoHead.appendChild(priority);
+
+    todo.appendChild(todoHead);
+
+    const desciption = document.createElement('p');
+    desciption.innerHTML = element.description;
+    expanded.appendChild(desciption);
+
+    todo.appendChild(expanded);
+
+    list.appendChild(todo);
+  });
+}
+
+// Expand todo to include desciption when clicked
+const list = document.getElementById('list');
+list.addEventListener('click', (e) => {
+  if (e.target && e.target.matches('#todoHead')) {
+    const expanded = e.target.nextElementSibling;
+    if (expanded.style.display === 'none') {
+      expanded.style.display = 'flex';
+    } else {
+      expanded.style.display = 'none';
+    }
+  }
+});
+
 // Create new todo
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -31,7 +83,8 @@ form.addEventListener('submit', (event) => {
   form.reset();
   // Hide form
   form.style.display = 'none';
-  // Update the list of books.
+  // Update the list of todos
+  listDisplay();
 });
 
 const addTodo = document.getElementById('addTodo');
--- src/todo.js
+++ src/todo.js
@@ -1,9 +1,15 @@
+import format from 'date-fns/format';
+
 class Todo {
   constructor(title, description, dueDate, priority) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
+  }
+
+  formatDate() {
+    return format(new Date(this.dueDate), 'dd/MM/yyyy');
   }
 }
 
